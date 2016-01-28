c672ef58cbf17dcae4e5a0f670a7e82075ffa73c
ErrantConstruct <errantconstruct@gmail.com>
2016-01-27 19:22:58 -0500

    Adding rating control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Matt Postema on 1/27/16.
+//  Copyright © 2016 Matt Postema. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+}
