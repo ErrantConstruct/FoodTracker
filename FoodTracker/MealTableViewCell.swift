20ba227e0f6a1277663313b487a583f2bb94cc7c
ErrantConstruct <errantconstruct@gmail.com>
2016-02-19 22:01:30 -0500

    Added custom table view cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Matt Postema on 2/19/16.
+//  Copyright © 2016 Matt Postema. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
